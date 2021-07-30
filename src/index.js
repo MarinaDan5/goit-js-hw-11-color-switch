7dbeb11b92662a426313695b0be897819e972e44
Maryna Danylchuk <marina123456mari@gmail.com>
2021-07-30 16:27:31 +0300

    добавляет разметку и JS

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+  body: document.querySelector('body'),
+  start: document.querySelector('[data-action="start"]'),
+  stop: document.querySelector('[data-action="stop"]'),
+}
+
+refs.start.addEventListener('click', () => {
+  changeColor.start();
+})
+
+refs.stop.addEventListener('click', () => {
+  changeColor.stop();
+})
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const changeColor = {
+  intervalId: null,
+  isActive: false,
+  
+  start() {
+    if (this.isActive) {
+      return
+     }
+    this.isActive = true;
+    this.intervalId = setInterval(() => {
+      refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+    }, 1000);
+  },
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+  }
+}
+
+
+
+
